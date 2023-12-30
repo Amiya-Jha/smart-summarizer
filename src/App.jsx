bae3fb773c02ad13e91f8f0fdbf0b7a39b48a9a7
Amiya-Jha <97188732+Amiya-Jha@users.noreply.github.com>
2023-12-30 17:23:13 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Upper from './components/Upper';
+import Below from './components/Below';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      <div className="main">
+      </div>
+
+      <div className="app">
+        <Upper/>
+        <Below/>
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import loader from './loader.svg'
+import copy from './copy.svg'
+import tick from './tick.svg'
+import mylogo from './mylogo.png'
+import link from './link.png'
+
+export {
+    loader,
+    copy,
+    tick,
+    mylogo,
+    link,
+}
